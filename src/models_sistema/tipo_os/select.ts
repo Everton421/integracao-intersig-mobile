a58e47bb7d85077239d12030fb4bd24270ba59d4
unknown <evertonjose421@gmail.com>
2025-05-16 14:21:23 -0300

    16052025

--- src/models_sistema/tipo_os/select.ts
+++ src/models_sistema/tipo_os/select.ts
@@ -1,4 +1,4 @@
-import { conn_mobie } from "../../database/databaseConfig";
+import { conn_mobie, conn_sistema } from "../../database/databaseConfig";
 import { ITipoOsSistema } from "./types/ITipoOsSistema";
 
 export class SelectTiposOsSistema{
@@ -8,7 +8,7 @@ export class SelectTiposOsSistema{
  
         let sql = ` select *
         from ${empresa}.tipos_os`
-            await conn_mobie.query(sql,  (err:any, result:ITipoOsSistema[]  )=>{
+            await conn_sistema.query(sql,  (err:any, result:ITipoOsSistema[]  )=>{
                 if (err)  reject(err); 
                   resolve(result)
             })
