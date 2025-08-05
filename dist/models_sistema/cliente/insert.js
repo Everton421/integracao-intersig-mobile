25df4ce1fc852c33e58d82c313025e924f051643
unknown <evertonjose421@gmail.com>
2025-08-05 15:38:44 -0300

    chore: Remove a pasta 'dist' do versionamento

--- dist/models_sistema/cliente/insert.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Insert_clientes = void 0;
-class Insert_clientes {
-}
-exports.Insert_clientes = Insert_clientes;
